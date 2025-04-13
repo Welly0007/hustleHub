1279e274b0dc04aac9d7af8a28ac61c49d901cd7
Welly0007 <1020066@stemoctober.edu.eg>
2025-04-13 16:57:00 +0200

    adjust for pages

--- scripts/preDisplayScript.js
+++ scripts/preDisplayScript.js
@@ -1,3 +1,10 @@
+// Dynamically set the base URL
+const base = document.createElement('base');
+base.href = window.location.hostname === 'welly0007.github.io' 
+    ? 'https://welly0007.github.io/hustleHub/' 
+    : '/';
+document.head.prepend(base);
+
 // Fetching Header and rendering to page
 fetch('pages/header.html')
     .then(res => res.text())
