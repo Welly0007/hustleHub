6a155a7c3f051b037693ec5241cffedadeb599ff
Welly0007 <1020066@stemoctober.edu.eg>
2025-04-13 15:38:52 +0200

    paths

--- scripts/preDisplayScript.js
+++ scripts/preDisplayScript.js
@@ -1,11 +1,11 @@
 // Fetching Header and rendering to page
-fetch('/../pages/header.html')
+fetch('../pages/header.html')
     .then(res => res.text())
     .then(data => {
         document.querySelector('.headcontainer').innerHTML = data;
     });
 // Fetching Footer
-fetch('/../pages/footer.html')
+fetch('../pages/footer.html')
     .then(res => res.text())
     .then(data => {
         document.querySelector('footer').innerHTML = data;
