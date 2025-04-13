549cce71b5cc712a94445eee67f6fcee415c5626
Welly0007 <1020066@stemoctober.edu.eg>
2025-04-13 15:26:51 +0200

    paths fixes, photos compressed

--- scripts/preDisplayScript.js
+++ scripts/preDisplayScript.js
@@ -1,11 +1,13 @@
-        //fetching Header and rendering to page
-        fetch('pages/header.html').then(res => res.text()).then(data =>{
-            document.querySelector('.headcontainer').innerHTML = data;
-        });
-        // fetching footer
-        fetch('pages/footer.html')
-        .then(res => res.text())
-        .then(data => {
+// Fetching Header and rendering to page
+fetch('/pages/header.html')
+    .then(res => res.text())
+    .then(data => {
+        document.querySelector('.headcontainer').innerHTML = data;
+    });
+// Fetching Footer
+fetch('/pages/footer.html')
+    .then(res => res.text())
+    .then(data => {
         document.querySelector('footer').innerHTML = data;
-        });
-        // comment
+    });
+// comment
