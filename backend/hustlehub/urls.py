f964023dc95d7ffeadeed98a9aa97935491f5a4e
Roba85 <roba.ahmed211@gmail.com>
2025-05-19 02:23:47 +0300

    Created the views directory

--- backend/hustlehub/urls.py
+++ backend/hustlehub/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import signup_view
-from .views import login_view
+from .views import signup_view, login_view, login, home, edit_job, contact, jobs, profile, admin_profile
+from .views import login_page, add_job, apply, job_details, submitted_successfully, forgot_password, signup
 from . import views
 from django.urls import re_path
 
