3d12b58f40dba9c70762d55a1068e676920f7e7b
Roba85 <roba.ahmed211@gmail.com>
2025-05-19 19:12:02 +0300

    Finished Models

--- backend/hustlehub/admin.py
+++ backend/hustlehub/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Jobs, JobType, Workplace, Career_level, Categories, Countries, Tags
 
 admin.site.register(CustomUser)
+admin.site.register(Tags)
+admin.site.register(Jobs)
+admin.site.register(JobType)
+admin.site.register(Workplace)
+admin.site.register(Career_level)
+admin.site.register(Categories)
+admin.site.register(Countries)
